c76063f594917e38d29f4315c1b903a6e93ac898
SpicyCookies <spicycookies8@gmail.com>
2020-08-29 22:33:02 -0500

    Added test covid case summary route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Covid Case Summary Endpoint
+  get 'covid-case-summary', to: 'covid_cases#index', format: 'json'
 end
