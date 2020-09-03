434e8152fc357ef5e1100b9ad81535a4f963e5e1
SpicyCookies <spicycookies8@gmail.com>
2020-09-02 20:05:33 -0500

    Fixed search mechanism and added observation comments

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class SearchesController < ApplicationController
+  # Observation: On an index without query params, the external API will only return a partial response.
+  # Observation: The default results on the external API are 20 results.
   def index
     # Assumption: A nil and empty string are the same query for the Brewery API
     state_param = search_params[:state].blank? ? nil : search_params[:state]
@@ -15,14 +17,14 @@ class SearchesController < ApplicationController
       search_instance.breweries
     else
       retrieved_breweries = Brewery.retrieve_breweries(
-        state: search_params[:state],
-        brewery_type: search_params[:brewery_type]
+        state: state_param,
+        brewery_type: brewery_type_param
       )
 
       # Save breweries into new unique search
       Search.create!(
-        state: search_params[:state],
-        brewery_type: search_params[:brewery_type],
+        state: state_param,
+        brewery_type: brewery_type_param,
         breweries: retrieved_breweries
       )
 
